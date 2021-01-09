b6118b3d620df594f8dbb16f2e2001f63d7b379e
Tim Mingxing <tim.tt24@gmail.com>
2021-01-09 12:17:05 +0800

    使用异步生成器实现B站直播间协议

--- BiliClient/__init__.py
+++ BiliClient/__init__.py
@@ -1,8 +1,12 @@
 
-__version__ = '1.1.9'
+__version__ = '1.2.0'
 
 from .asyncBiliApi import asyncBiliApi as asyncbili
+from .asyncXliveWs import asyncXliveRoomMsgGenerator as asyncXliveRoomMsgGenerator
+from .asyncXliveWs import asyncXliveRoomMsgGeneratorMulti as asyncXliveRoomMsgGeneratorMulti
 
 __all__ = (
     'asyncbili',
+    'asyncXliveRoomMsgGenerator',
+    'asyncXliveRoomMsgGeneratorMulti'
 )
