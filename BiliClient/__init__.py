224e81522fa7235c0fecfb1345c235a6455ee362
Tim Mingxing <tim.tt24@gmail.com>
2021-01-14 18:22:36 +0800

    优化直播心跳

--- BiliClient/__init__.py
+++ BiliClient/__init__.py
@@ -1,11 +1,13 @@
 
 __version__ = '1.2.0'
 
+from .wasm_enc import calc_sign
 from .asyncBiliApi import asyncBiliApi as asyncbili
 from .asyncXliveWs import asyncXliveRoomMsgGenerator as asyncXliveRoomMsgGenerator
 from .asyncXliveWs import asyncXliveRoomMsgGeneratorMulti as asyncXliveRoomMsgGeneratorMulti
 
 __all__ = (
+    'calc_sign',
     'asyncbili',
     'asyncXliveRoomMsgGenerator',
     'asyncXliveRoomMsgGeneratorMulti'
