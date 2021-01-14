1c8818f490de676dc168e4d85c5d29d72271c756
Tim Mingxing <tim.tt24@gmail.com>
2021-01-14 15:04:04 +0800

    优化天选时刻的关注和取关

--- tasks/xlive_anchor_task.py
+++ tasks/xlive_anchor_task.py
@@ -1,63 +1,53 @@
 from BiliClient import asyncbili
 from typing import AsyncGenerator, Dict, Any, List, Union
-import logging, asyncio
+import logging, asyncio, time, traceback
 from aiohttp.client_exceptions import ServerDisconnectedError
 from async_timeout import timeout
 
 async def xlive_anchor_task(biliapi: asyncbili,
-                                   task_config: Dict[str, Any]
-                                   ) -> None:
+                            task_config: Dict[str, Any]
+                            ) -> None:
     Timeout = task_config.get("timeout", 850)
     delay = task_config.get("delay", 0)
     save_map = {} #id:(roomid, uid)
+    is_followed = True
     try:
         async with timeout(Timeout):
-            async for room in searcheRoomGenerator(biliapi, task_config["searche_interval"], task_config["searche_areas"]):
-                if '2' in room["pendant_info"] and room["pendant_info"]["2"]["pendent_id"] == 504:
-                    if delay:
-                        await asyncio.sleep(delay)
-                    try:
-                        ret = await biliapi.getLotteryInfoWeb(room["roomid"])
-                    except Exception as e:
-                        logging.warning(f'{biliapi.name}: 天选时刻抽奖任务获取直播间{room["roomid"]}抽奖信息异常，原因为({str(e)})')
-                        continue
-                    if ret["code"] != 0:
-                        logging.warning(f'{biliapi.name}: 天选时刻抽奖任务获取直播间{room["roomid"]}抽奖信息失败，信息为({ret["message"]})')
-                        continue
-                    anchor = ret["data"]["anchor"]
+            while True:
+                for area in task_config["searche_areas"]:
+                    async for room in xliveRoomGenerator(biliapi, area["paid"], area["aid"], area["sort"], area["ps"]):
+                        if '2' in room["pendant_info"] and room["pendant_info"]["2"]["pendent_id"] == 504: #判断房间是否有天选时刻
+                            if delay:
+                                await asyncio.sleep(delay)
 
-                    if anchor["status"] != 1: #排除重复参加
-                        continue
+                            bl, anchor = await getAnchorInfo(biliapi, room["roomid"]) #获取天选信息
+                            if not bl:
+                                continue
 
-                    if anchor["id"] in save_map: #排除重复参加
-                        continue
+                            if anchor["status"] != 1: #排除重复参加
+                                continue
 
-                    if not isJoinAnchor(anchor, task_config):
-                        save_map[anchor["id"]] = None
-                        continue
-                    status = False
-                    try:
-                        ret = await biliapi.xliveAnchorJoin(anchor["id"], anchor["gift_id"], anchor["gift_num"])
-                    except Exception as e:
-                        logging.warning(f'{biliapi.name}: 参与直播间{room["roomid"]}的天选时刻{anchor["id"]}异常，原因为({str(e)})')
-                    else:
-                        if ret["code"] == 0:
-                            save_map[anchor["id"]] = (room["roomid"], room["uid"])
-                            logging.info(f'{biliapi.name}: 参与直播间{room["roomid"]}的天选时刻{anchor["id"]}({anchor["award_name"]})成功')
-                            status = True
-                        else:
-                            save_map[anchor["id"]] = None
-                            logging.warning(f'{biliapi.name}: 参与直播间{room["roomid"]}的天选时刻{anchor["id"]}失败，信息为({ret["message"]})')
+                            if anchor["id"] in save_map: #排除重复参加
+                                continue
 
-                    if task_config["unfollow"] and status and anchor["require_type"] == 1:
-                        await asyncio.sleep(6)
-                        await biliapi.followUser(room["uid"], 0)
-                        logging.info(f'{biliapi.name}: 取关主播{room["uid"]}')
+                            if not isJoinAnchor(anchor, task_config): #过滤条件
+                                save_map[anchor["id"]] = None
+                                continue
+
+                            if task_config["unfollow"]:
+                                is_followed = await isUserFollowed(biliapi, room["uid"])
+
+                            await anchorJoin(biliapi, anchor, room, is_followed, save_map) #参加天选时刻
+
+                await asyncio.sleep(task_config["searche_interval"])
+                await cleanMapWithUnfollow(biliapi, save_map)
     
     except asyncio.TimeoutError:
-        logging.warning(f'{biliapi.name}: 天选时刻抽奖任务超时({Timeout}秒)退出')
+        logging.warning(f'{biliapi.name}: 天选时刻抽奖任务超时({Timeout}秒)')
     except Exception as e:
-        logging.warning(f'{biliapi.name}: 天选时刻抽奖任务异常，原因为({str(e)})，退出任务')
+        logging.warning(f'{biliapi.name}: 天选时刻抽奖任务异常，异常为({traceback.format_exc()})')
+
+    await cleanMapWithUnfollow(biliapi, save_map, True)
 
 def isJoinAnchor(anchor: Dict[str, Any], 
                  condition: Dict[str, Any]
@@ -78,18 +68,64 @@ def isJoinAnchor(anchor: Dict[str, Any],
             return False
     return True
 
-async def searcheRoomGenerator(biliapi: asyncbili,
-                               searche_interval: int,
-                               searche_areas: List[Dict[str, Union[str, int]]],
-                               ) -> AsyncGenerator:
-    '''
-    循环获取直播间(生成器)
-    '''
-    while True:
-        for area in searche_areas:
-            async for room in xliveRoomGenerator(biliapi, area["paid"], area["aid"], area["sort"], area["ps"]):
-                yield room
-        await asyncio.sleep(searche_interval)
+async def isUserFollowed(biliapi: asyncbili, 
+                   uid: int
+                   ) -> bool:
+    '''判断是否关注用户'''
+    try:
+        ret = await biliapi.getRelationByUid(uid)
+    except Exception as e:
+        logging.warning(f'{biliapi.name}: 天选判断与用户{uid}的关注状态失败，原因为({str(e)})，默认未关注')
+        return False
+    else:
+        if ret["code"] == 0:
+            return ret["data"]["attribute"] == 2
+        else:
+            logging.warning(f'{biliapi.name}: 天选判断与用户{uid}的关注状态失败，原因为({ret["message"]})，默认未关注')
+            return False
+
+async def anchorJoin(biliapi: asyncbili, 
+                     anchor: dict,
+                     room: dict,
+                     is_followed: bool,
+                     save_map: dict
+                     ):
+    '''参加天选时刻'''
+    try:
+        ret = await biliapi.xliveAnchorJoin(anchor["id"], anchor["gift_id"], anchor["gift_num"])
+    except Exception as e:
+        logging.warning(f'{biliapi.name}: 参与直播间{room["roomid"]}的天选时刻{anchor["id"]}异常，原因为({str(e)})')
+    else:
+        if ret["code"] == 0:
+            save_map[anchor["id"]] = (room["roomid"], room["uid"], anchor["current_time"]+anchor["time"],not is_followed and anchor["require_type"] == 1)
+            logging.info(f'{biliapi.name}: 参与直播间{room["roomid"]}的天选时刻{anchor["id"]}({anchor["award_name"]})成功')
+
+async def cleanMapWithUnfollow(biliapi: asyncbili, 
+                        save_map: dict,
+                        clean_all: bool = False
+                        ) -> bool:
+    now_time = int(time.time())
+    for k in list(save_map.keys()):
+        if save_map[k]:
+            if clean_all or now_time > save_map[k][2]:
+                if save_map[k][3]:
+                    await biliapi.followUser(save_map[k][1], 0)
+                    logging.info(f'{biliapi.name}: 取关主播{save_map[k][1]}')
+                del save_map[k]
+
+async def getAnchorInfo(biliapi: asyncbili, 
+                        room_id: int
+                        ) -> (bool, dict):
+    '''获取房间天选时刻信息'''
+    try:
+        ret = await biliapi.getLotteryInfoWeb(room_id)
+    except Exception as e:
+        logging.warning(f'{biliapi.name}: 天选时刻抽奖任务获取直播间{room_id}抽奖信息异常，原因为({str(e)})')
+        return False, None
+    if ret["code"] != 0:
+        logging.warning(f'{biliapi.name}: 天选时刻抽奖任务获取直播间{room["roomid"]}抽奖信息失败，信息为({ret["message"]})')
+        return False, None
+    return bool(ret["data"]["anchor"]), ret["data"]["anchor"]
 
 async def xliveRoomGenerator(biliapi: asyncbili,
                              pAreaId: int,
